01b87f6653e0d72c17a3d6eb20af830993bc9673
cankush625 <cankush625@gmail.com>
2019-05-19 22:55:49 +0530

    SQL basic concepts program is updated

--- SQL/test.sql
+++ SQL/test.sql
@@ -60,3 +60,36 @@ select * from verifiedTweets;
 insert into verifiedTweets(Name) values('Roshan');
 
 select * from verifiedTweets;
+
+/*New table created*/
+create table verifiedTweets3(
+	ID int default 0,
+    Name varchar(20) default 'not provided',
+    Tweets int default 0
+);
+
+show tables;
+
+desc verifiedTweets3;
+
+insert into verifiedTweets3(ID) values(1);
+
+select * from verifiedtweets3;
+
+insert into verifiedTweets3() values();
+
+select * from verifiedtweets3;
+
+/* null and default values are defined at a time*/
+create table verifiedTweets4(
+	ID int not null default 0,
+    Name varchar(20) not null default 'not provided',
+    Tweets int not null default 0
+);
+
+desc verifiedTweets4;
+
+insert into verifiedTweets4(ID) values(null); /*This throws an error because 
+												we defined ID not to be null*/
+                                                
+/*by Ankush Chavan*/
