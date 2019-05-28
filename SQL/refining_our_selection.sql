ca13b7c085d361a35dd9fd6621ecb5e67c24569d
cankush625 <cankush625@gmail.com>
2019-05-28 23:24:32 +0530

    Code to limit the number of results using limit is added

--- SQL/refining_our_selection.sql
+++ SQL/refining_our_selection.sql
@@ -43,3 +43,21 @@ select title, author_fname, author_lname from books order by 2;
 /*Sometimes it is happen that john comes before james because we have sorted using onky the last name and
 not by using both last name and first name. To overcome this we are sorting using both the fields*/
 select title, author_fname, author_lname from books order by author_fname, author_lname;
+
+/*limit*/
+/*Limit the number of results that we are selecting*/
+select * from books limit 3;
+select title from books limit 5;
+
+/*Limit the results using order by with limit*/
+select title, released_year from books order by released_year desc limit 3;
+
+/*limit by specifying starting point and how many results we want*/
+select title, released_year from books order by released_year desc limit 0, 5;
+select title, released_year from books order by released_year desc limit 2, 5;
+
+/*To choose item at specific index*/
+select title, released_year from books order by released_year desc limit 8, 1;
+
+/*To get the items from starting index to the end of the table we specify a giant number*/
+select title, released_year from books limit 5, 1442545533334;
