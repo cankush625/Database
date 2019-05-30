c0328d734664b6c5e45a04988c09299654fbff35
cankush625 <cankush625@gmail.com>
2019-05-30 23:25:14 +0530

    Code for group by is added

--- SQL/aggregate_functions.sql
+++ SQL/aggregate_functions.sql
@@ -12,3 +12,18 @@ select count(distinct author_fname, author_lname) as 'Number of Authors' from bo
 
 /*Using count with like*/
 select count(title) as "Title containing 'the'" from books where title like '%the%';
+
+/*group by*/
+/*It groups the results under the mentioned field and shows only first record*/
+select title, author_lname from books group by author_lname;
+
+/*Using group by with count*/
+/*Count how many books each author have written*/
+select author_lname, count(*) as 'Books by each author' from books group by author_lname;
+select author_fname, author_lname, count(*) as 'Books by each author' from books group by author_fname, author_lname;
+
+/*Using group by with count and order by*/
+select released_year, count(*) as 'Books released in year' from books group by released_year order by released_year;
+
+/*Using group by with concat, count and order by*/
+select concat('In ', released_year, ', ', count(*), ' book(s) are released!') as 'Books released in year' from books group by released_year order by released_year;
